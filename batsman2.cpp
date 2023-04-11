906ea34baf7dabbc211ac9ffb10c18e145f2db86
payal mulani <payalmulani402@gmail.com>
2023-04-11 16:55:33 +0530

    c++

---
+++ batsman2.cpp
@@ -0,0 +1,11 @@
+#inclued<iostream>
+using namespace std;
+class batsman
+{
+    private :
+    int bcode;
+    char bname[20];
+    int innings,notout,runs;
+    float batavg;
+    float calcavg f
+}

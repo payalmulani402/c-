f2d00c0c04fbaff80a8b73c0fa8abaab96eed774
payal mulani <payalmulani402@gmail.com>
2023-04-14 15:09:21 +0530

    report.cpp commit

--- report.cpp
+++ report.cpp
@@ -5,10 +5,17 @@ class report
     private:
     int adno;
     char name[20];
-    float marks;
+    float marks [5];
+    float total;
+    float avg;
+    void GETAVG()
     {
-
-
+        float sum=0;
+        for(int i=1;i<=5;i++)
+        {
+            sum = sum + marks[i];
+        }
+       avg = sum/5;
     }
     public:
     void READINFO()
@@ -16,11 +23,13 @@ class report
         cout<<"enter adno:";
         cin>>adno;
         cout<<"enter name:";
-        cin>>name
-        cout<<"enter marks:";
-        cin>>marks;
-
-
+        cin>>name;
+        cout<<"enter 5 sub of marks:"<<endl;
+        for (int i=1;i<=5;i++)
+        {
+        cin>>marks[i];
+        }
+            GETAVG();
     }
 
     void DISPLAYINFO()
@@ -28,16 +37,21 @@ class report
     
         cout<<"adno:"<<adno<<endl;
         cout<<"name:"<<name<<endl;
-        cout<<"marks:"<<marks<<endl;
-}
+        cout<<"enter 5 subject of marks: " ;
+          for(int i=1;i<=5;i++)
+          {
+            cout<<marks[i]<<"";
+          }
+          cout<<"\nAverage:"<< avg<<endl;
+ }
 
 };
 
 int main()
 {
-      report a;
-      a.READINFO();
-      a.DISPLAYINFO();
+      report r;
+      r.READINFO();
+      r.DISPLAYINFO();
 }
 
 
